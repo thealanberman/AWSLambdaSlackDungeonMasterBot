54d5ef17ae7467c1c00e1453c02042f7286af579
Al <thealanberman@gmail.com>
2016-06-25 11:50:41 -0700

    basic echo functionality

---
+++ AWSLambdaSlackDungeonMasterBot.js
@@ -0,0 +1,32 @@
+// This is the Slack Token generated when creating the slash command
+var token = "";
+var qs = require('querystring');
+
+exports.handler = function (event, context) {
+    if (token !== null) {
+        processEvent(event, context);
+    } else {
+        context.fail("Token has not been set.");
+    }
+};
+
+var processEvent = function(event, context) {
+    var body = event.body;
+    var params = qs.parse(body);
+    var requestToken = params.token;
+    if (requestToken !== token) {
+        console.error("Request token (" + requestToken + ") does not match exptected");
+        context.fail("Invalid request token");
+    }
+
+    var user = params.user_name;
+    var command = params.command;
+    var channel = params.channel_name;
+    var commandText = params.text;
+
+    context.succeed(user + " invoked " + command + " in " + channel + " with the following text: " + commandText);
+};
+
+var parseCommand = function (text) {
+
+}
